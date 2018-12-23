dd68ef5a75aea465f879b21cf0d9bcc359dd0542
samromeu <45852159+samromeu@users.noreply.github.com>
2018-12-23 16:21:53 +0000

    task_A picsave
    Iterates through all pictures and places the ones containing a face in a separate folder

--- task_1.py
+++ task_1.py
@@ -7,25 +7,27 @@ Created on Sat Dec 22 16:15:55 2018
 
 import cv2
 import os
+import csv
+
 datasetDirectory = 'C:\\Users\\Sam\\Desktop\\UNI\\Fourth Year\\Machine learning\\Assignment'
+faceDirectory = 'C:\\Users\\Sam\\Desktop\\UNI\\Fourth Year\\Machine learning\\Assignment\\faces'
 dataset = []
+csvData = []
 
-def findFaces(image):
-    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
+def findFace(image):
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
     # Read the image
-    grayimage = cv2.imread(image,0)
-    
+    grayimage = cv2.imread(image,0)    
     # Detect faces in the image
-    faces = faceCascade.detectMultiScale(grayimage, scaleFactor=1.1, minNeighbors=1)
-    
+    faces = faceCascade.detectMultiScale(grayimage, scaleFactor=1.05, minNeighbors=3)   
     return len(faces)
 
 for filename in os.listdir(datasetDirectory):
     if filename.endswith('.png'):
-        if findFaces(filename) > 0:
+        if findFace(filename) > 0:
             print(filename,"contains a face")
-            dataset.append(filename)
+            image = cv2.imread(filename)
+            cv2.imwrite(os.path.join(faceDirectory , filename), image)
         else:
             print(filename, "does not contain a face")
             
