fcf9db6b3fdc8c5e8076bd3e04b027543b641d26
samromeu <45852159+samromeu@users.noreply.github.com>
2019-01-10 17:00:15 +0000

    Made it so directories were universal (e.g. not just C://SamsPC)

--- splitData.py
+++ splitData.py
@@ -9,26 +9,29 @@ import cv2
 import csv
 import os
 import numpy as np
-from tqdm import tqdm
+from tqdm import tqdm 
 
-datasetDirectory = 'C:\\Users\\Sam\\Desktop\\UNI\\Fourth Year\\Machine learning\\Assignment\\faces'
-trainingDirectory = 'C:\\Users\\Sam\\Desktop\\UNI\\Fourth Year\\Machine learning\\Assignment\\training'
-testingDirectory = 'C:\\Users\\Sam\\Desktop\\UNI\\Fourth Year\\Machine learning\\Assignment\\testing'
+datasetDirectory = './faces'
+trainingDirectory = './training'
+testingDirectory = './testing'
 
-splitRatio = 0.8
+splitRatio = 0.8 #Sets the training:test ratio at 80:20
 
-train_thresh = round(splitRatio * len(os.listdir(datasetDirectory)))
+train_thresh = round(splitRatio * len(os.listdir(datasetDirectory))) #Calculates the number of images in the training set
 
-for i in range(len(os.listdir(datasetDirectory))):
-    filename = os.listdir(datasetDirectory)[i]
-    if filename.endswith('.png'):
-        if i < train_thresh:
-            image = cv2.imread(filename)
-            currentDir = "training"
-            cv2.imwrite(os.path.join(trainingDirectory , filename), image)
-        elif i > train_thresh and i <= len(os.listdir(datasetDirectory)):
-            image = cv2.imread(filename)
-            currentDir = "testing"
-            cv2.imwrite(os.path.join(testingDirectory , filename), image)
+for i in range(len(os.listdir(datasetDirectory))): #Iterates through every file in the face directory
+    filename = os.listdir(datasetDirectory)[i] #Indexes every file with number 'i'
     
-        print("Saving", filename, "to", currentDir)
+    if filename.endswith('.png'): #Only applies the following code to png images
+        if i < train_thresh: #For all images below the training threshold (80% of images)
+            image = cv2.imread(filename) #Read in image
+            currentDir = "training"
+            cv2.imwrite(os.path.join(trainingDirectory , filename), image) #Save image to the training directory folder
+        
+        elif i >= train_thresh and i <= len(os.listdir(datasetDirectory)): #For all images above training threshold (20% of images)
+            image = cv2.imread(filename) #Read in image
+            currentDir = "testing"
+            cv2.imwrite(os.path.join(testingDirectory , filename), image) #Save image to testing directory folder
+    
+        print("Saving", filename, "to", currentDir) #Print the current image being saved and what folder it is being saved to
+
